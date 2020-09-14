58acc1bdd4d16c5faf89513de49dfc958d16cdf0
alexiyalembic <alexiysc@gmail.com>
2020-09-14 08:55:35 +0700

    content update

---
+++ precache-manifest.c82e7371258d0c279039b2bb91c5c3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "643e29fbed40b6986bf1",
+    "url": "/react-resume/static/css/main.7c0b67a1.chunk.css"
+  },
+  {
+    "revision": "643e29fbed40b6986bf1",
+    "url": "/react-resume/static/js/main.ecd62ea0.chunk.js"
+  },
+  {
+    "revision": "9ebccc51354bc6ce9a39",
+    "url": "/react-resume/static/js/runtime~main.bbd03709.js"
+  },
+  {
+    "revision": "9d2c421085aee16aba46",
+    "url": "/react-resume/static/js/2.a67fb30d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "65ea64ea4aae530402f8028a9a02f4fb",
+    "url": "/react-resume/index.html"
+  }
+];
